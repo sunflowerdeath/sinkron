c68021b2bf6d820d12392b284a8cd198013a0389
Molecular Man <sunflowerdeath@gmail.com>
2024-11-29 19:40:24 +0300

    fix

--- packages/front/src/env.ts
+++ packages/front/src/env.ts
@@ -3,9 +3,9 @@ const isProductionEnv = process.env.NODE_ENV === "production"
 const tauri = window.location.hostname === "tauri.localhost"
 
 const wsUrl = isProductionEnv
-    ? "wss://sync.sinkron.xyz"
+    ? "wss://sync.sinkron.xyz/sync"
     : tauri
-    ? "ws://10.0.2.2:80"
+    ? "ws://10.0.2.2:3333/sync"
     : `ws://${window.location.hostname}:3333/sync`
 
 const apiUrl = isProductionEnv
